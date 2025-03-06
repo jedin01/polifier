2ce0f1f43faf4003e1a8639f63593037981b23e8
jedin01 <abnerjaredejede@gmail.com>
2025-03-06 12:03:28 +0100

    mar06:12:03

--- src/Polifier.java
+++ src/Polifier.java
@@ -1,19 +1,36 @@
-import java.awt.image.PackedColorModel;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Polifier {
 
 
     private final String expressao;
-    private Pattern regex;
+    private List<Pattern> regex;
     public Polifier(String expressao){
         this.expressao = expressao;
+        this.regex = new ArrayList<Pattern>();
+        this.simplificar();
     }
     public void selectRegex(){
-        switch (new IdentificadorDeEquacoesPolinomiais(this.expressao).getMaiorGrau()){
-            case 1:
-                this.regex = new Pattern("");
+        IdentificadorDeEquacoesPolinomiais funcao = new IdentificadorDeEquacoesPolinomiais(this.expressao);
+
+        if(funcao.getMaiorGrau()==1){
+            this.regex.add(Pattern.compile("(\\d*\\.?\\d*?)x(\\^[" + funcao.getMaiorGrau() + "])"));
+            this.regex.add(Pattern.compile("(\\d*\\.?\\d*?)x(\\^[" + funcao.getMaiorGrau() - 1 + "])"));
         }
+        else if (funcao.getMaiorGrau()==2) {
+            this.regex.add(Pattern.compile("(\\d*\\.?\\d*?)x(\\^["+funcao.getMaiorGrau()+"])"));
+            this.regex.add(Pattern.compile("(\\d*\\.?\\d*?)x(\\^["+funcao.getMaiorGrau()-1+"])"));
+        }
+
+
+    }
+    public void simplificar(){
+
+    }
+    public String getSimplified(){
+        return this.expressao;
     }
 
 }
